c608d0ef7cf19b9381bcceb37e430be300858858
hrr <jia@qiu.io>
2024-01-15 22:13:51 +0800

    3.9

--- exer3/index.js
+++ exer3/index.js
@@ -2,7 +2,9 @@ const http = require('http')
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json())
 app.use(
     morgan(function (tokens, req, res) {
