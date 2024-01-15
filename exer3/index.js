66b4f45fee7460bf586adbaf6a1dec633a725092
hrr <jia@qiu.io>
2024-01-15 23:15:33 +0800

    3.16

--- exer3/index.js
+++ exer3/index.js
@@ -25,8 +25,21 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons', (request, response) => {
-    Person.find({}).then(persons => response.json(persons))
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
+})
 
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -60,10 +73,10 @@ app.post('/api/persons', (request, response) => {
 
 app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndDelete(request.params.id)
-    .then(result => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 const PORT = process.env.PORT
