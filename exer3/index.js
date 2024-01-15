9d13816339a04c8960681bb8be585b5a62e789df
hrr <jia@qiu.io>
2024-01-15 23:00:23 +0800

    3.13

--- exer3/index.js
+++ exer3/index.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
 const http = require('http')
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 
 app.use(cors())
 app.use(express.json())
@@ -18,36 +20,14 @@ app.use(
         ].join(' ')
     }))
 
-let persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]
-
 app.get('/info', (request, response) => {
     //   response.send({ personsLength: persons.length, requestTime: Date.now() })
     response.send(`Phonebook has info for ${persons.length} people<br/>${new Date()}`)
 })
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => response.json(persons))
+
 })
 
 app.post('/api/persons', (request, response) => {
@@ -89,7 +69,7 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
-const PORT = 3002
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
