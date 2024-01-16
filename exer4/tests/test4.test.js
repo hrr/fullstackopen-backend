ae62d8df15b7109237c95b118a962ef04769d8a9
hrr <jia@qiu.io>
2024-01-16 12:01:02 +0800

    4.6

--- exer4/tests/test4.test.js
+++ exer4/tests/test4.test.js
@@ -1,3 +1,4 @@
+const { last } = require('lodash')
 const listHelper = require('../utils/list_helper')
 
 describe('total likes', () => {
@@ -15,6 +16,64 @@ describe('total likes', () => {
   test('when list has only one blog, equals the likes of that', () => {
     const result = listHelper.totalLikes(listWithOneBlog)
     expect(result).toBe(5)
+  })
+})
+
+describe('mostBlogs', () => {
+  const listWithBlogs = [
+    {
+      _id: '5a422aa71b54a676234d17f9',
+      title: 'Go To Statement Considered Harmfula',
+      author: 'Edsger W. Dijkstraa',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmfula.html',
+      likes: 10,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f9',
+      title: 'Go To Statement Considered Harmfula',
+      author: 'Edsger W. Dijkstraa',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmfula.html',
+      likes: 12,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f9',
+      title: 'Go To Statement Considered Harmfula',
+      author: 'Edsger W. Dijkstraa',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmfula.html',
+      likes: 11,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f9',
+      title: 'Go To Statement Considered Harmfula',
+      author: 'Edsger W. Dijkstraaa',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmfula.html',
+      likes: 11,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f9',
+      title: 'Go To Statement Considered Harmfula',
+      author: 'Edsger W. Dijkstraaa',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmfula.html',
+      likes: 11,
+      __v: 0
+    }
+  ]
+
+  test('when list has many blogs, return the top blogger with most blogs', () => {
+    const result = listHelper.mostBlogsAuthor(listWithBlogs)
+    expect(result.author).toEqual('Edsger W. Dijkstraa')
   })
 })
 
@@ -54,9 +113,8 @@ describe('favoriteBlog', () => {
     }
   ]
 
-  test('when list has only one blog, equals the likes of that', () => {
-    const result = listHelper.sortLikes(listWithBlogs, -1)[0]
-    expect(result.likes).toEqual(12)
+  test('when list has many blogs, equals the likes of the highest one', () => {
+    expect(listHelper.mostLikesBlog(listWithBlogs).likes).toEqual(12)
   })
 })
 
--- exer4/utils/list_helper.js
+++ exer4/utils/list_helper.js
@@ -1,10 +1,21 @@
 const { sum } = require('../utils/helpers')
+const _ = require('lodash')
 const dummy = (blogs) => {
     return 1
 }
 
-const sortLikes = (blogs, asc) => {
-    return blogs.sort((x, y) => (x.likes - y.likes) * asc)
+const sort = (blogs, entities) => {
+    const sortedBlogs = _.sortBy(blogs, [entities])
+    return sortedBlogs
+}
+
+const mostLikesBlog = (blogs) => {
+    return sort(blogs, ['likes'])[blogs.length - 1]
+}
+
+const mostBlogsAuthor = (blogs) => {
+    const authorBlogs = sort(_.values(_.groupBy(blogs.map(x => x.author))).map(d => ({author: d[0], blogs: d.length})), 'blogs')
+    return authorBlogs[authorBlogs.length - 1]
 }
 
 const totalLikes = (blogs) => {
@@ -15,5 +26,6 @@ const totalLikes = (blogs) => {
 module.exports = {
     dummy,
     totalLikes,
-    sortLikes
+    mostLikesBlog,
+    mostBlogsAuthor
 }
