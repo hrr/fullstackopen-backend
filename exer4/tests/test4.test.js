b440a38d3421e0ad8d9832de7e2f4aac507c827e
hrr <jia@qiu.io>
2024-01-16 13:29:30 +0800

    4.7

--- exer4/tests/test4.test.js
+++ exer4/tests/test4.test.js
@@ -19,6 +19,64 @@ describe('total likes', () => {
   })
 })
 
+describe('mostLikes', () => {
+  const listWithBlogs = [
+    {
+      _id: '0',
+      title: 'Go To Statement Considered Harmfula',
+      author: 'Edsger W. Dijkstraa',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmfula.html',
+      likes: 1,
+      __v: 0
+    },
+    {
+      _id: '1',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '2',
+      title: 'Go To Statement Considered Harmfula',
+      author: 'Edsger W. Dijkstraa',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmfula.html',
+      likes: 2,
+      __v: 0
+    },
+    {
+      _id: '3',
+      title: 'Go To Statement Considered Harmfula',
+      author: 'Edsger W. Dijkstraa',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmfula.html',
+      likes: 11,
+      __v: 0
+    },
+    {
+      _id: '4',
+      title: 'Go To Statement Considered Harmfula',
+      author: 'Edsger W. Dijkstraaa',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmfula.html',
+      likes: 11,
+      __v: 0
+    },
+    {
+      _id: '5',
+      title: 'Go To Statement Considered Harmfula',
+      author: 'Edsger W. Dijkstraaa',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmfula.html',
+      likes: 11,
+      __v: 0
+    }
+  ]
+
+  test('when list has many blogs, return the blogger with most likes', () => {
+    const result = listHelper.mostLikesAuthor(listWithBlogs)
+    expect(result.author).toEqual('Edsger W. Dijkstraaa')
+  })
+})
+
 describe('mostBlogs', () => {
   const listWithBlogs = [
     {
--- exer4/utils/list_helper.js
+++ exer4/utils/list_helper.js
@@ -13,9 +13,29 @@ const mostLikesBlog = (blogs) => {
     return sort(blogs, ['likes'])[blogs.length - 1]
 }
 
+const mostLikesAuthor = (blogs) => {
+    const authorLikes = totalAuthorLikes(blogs)
+    return sort(authorLikes, ['likes'])[authorLikes.length - 1]
+}
+
 const mostBlogsAuthor = (blogs) => {
-    const authorBlogs = sort(_.values(_.groupBy(blogs.map(x => x.author))).map(d => ({author: d[0], blogs: d.length})), 'blogs')
+    const authorBlogs = sort(_.values(_.groupBy(blogs.map(x => x.author))).map(d => ({ author: d[0], blogs: d.length })), 'blogs')
     return authorBlogs[authorBlogs.length - 1]
+}
+
+const uniqueAuthors = (blogs) => {
+    const uniqueAuthors = _.map(blogs, 'author').filter((value, index, array) => { return array.indexOf(value) === index })
+    return uniqueAuthors
+}
+
+const totalAuthorLikes = (blogs) => {
+    const sumByAuthor = blogs.reduce((final, curr) => {
+        if (!final[curr.author]) final[curr.author] = curr.likes
+        else final[curr.author] += curr.likes
+        return final
+    }, {})
+
+    return Object.entries(sumByAuthor).map(x => ({ 'author': x[0], 'likes': x[1] }))
 }
 
 const totalLikes = (blogs) => {
@@ -27,5 +47,7 @@ module.exports = {
     dummy,
     totalLikes,
     mostLikesBlog,
-    mostBlogsAuthor
+    mostBlogsAuthor,
+    totalAuthorLikes,
+    mostLikesAuthor
 }
