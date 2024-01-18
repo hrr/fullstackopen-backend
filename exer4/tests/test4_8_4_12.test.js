28e2908c59357565415599dfe0a925f9039a10ee
hrr <jia@qiu.io>
2024-01-18 11:50:42 +0800

    4.9 shouldn't this check for property and not object existence?

--- exer4/tests/test4_8_4_12.test.js
+++ exer4/tests/test4_8_4_12.test.js
@@ -29,3 +29,15 @@ test('4_8_list_blogs', async () => {
         .expect('Content-Type', /application\/json/)
     expect(resp.body).toHaveLength(initBlogs.length)
 })
+
+test('4_9_blog_ids', async () => {
+    const resp = await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+    resp.body.forEach(blog => {
+        expect(blog).toHaveProperty('id')
+    })
+
+    expect(resp.body).toBeDefined()
+})
