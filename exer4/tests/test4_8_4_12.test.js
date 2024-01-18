8be8fd8e12f048c0a7eeb874f59c43ce63e4e42e
hrr <jia@qiu.io>
2024-01-18 11:32:10 +0800

    4.8 part 1

---
+++ exer4/tests/test4_8_4_12.test.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+const { initialBlogs } = require('../utils/test_helper')
+
+const api = supertest(app)
+let initBlogs = []
+
+beforeEach(async () => {
+    initBlogs = initialBlogs()
+    await Blog.deleteMany({})
+    console.log('Deleted blogs')
+
+    let blogO = new Blog(initBlogs[0])
+    
+    await blogO.save()
+    console.log('Saved blog 1')
+
+    blogO = new Blog(initBlogs[1])
+    await blogO.save()
+    console.log('Saved blog 2')
+})
+
+test('4_8_list_blogs', async () => {
+    const resp = await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+      
+    expect(resp.body).toHaveLength(initBlogs.length)
+})
---
+++ exer4/utils/test_helper.js
@@ -0,0 +1,17 @@
+const initialBlogs = () => {
+    const blogs = [
+        {
+            content: 'Testers Test 1',
+            important: true
+        },
+        {
+            content: 'Testers Test 2',
+            important: false
+        }
+    ]
+    return blogs
+}
+
+module.exports = {
+    initialBlogs
+}
